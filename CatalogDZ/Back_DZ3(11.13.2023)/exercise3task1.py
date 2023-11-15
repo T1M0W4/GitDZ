a91f5623df20a29f03877b1846ef8e582b3dc6df
T1M0W4 <150163671+T1M0W4@users.noreply.github.com>
2023-11-15 06:08:18 +0600

    DZ3(11.15.2023)v0.3

--- CatalogDZ/Back_DZ3(11.13.2023)/exercise3task1.py
+++ CatalogDZ/Back_DZ3(11.13.2023)/exercise3task1.py
@@ -1,6 +1,6 @@
 user_in = input("Сколько Вам лет?: ")
 
 user_number = int(user_in)
-user_number = user_number ** 2
+user_number = pow(user_number, 2)
 
 print("Ваш возвраст, возведеная в степень 2 = ", user_number)
