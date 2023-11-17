25de47c16d88288094e3675d7fdb0e312c2b7ccb
T1M0W4 <150163671+T1M0W4@users.noreply.github.com>
2023-11-17 07:59:38 +0600

    DZ4(14.11.2023)v0.7

--- CatalogDZ/Back_DZ4(11.14.2023)/Task(7).py
+++ CatalogDZ/Back_DZ4(11.14.2023)/Task(7).py
@@ -2,4 +2,4 @@ thre_dig_num = int(input("Введите трехзначное число: "))
 
 sec_num = (thre_dig_num // 10 % 10)
 
-print("Ваше двухзначное число: ", thre_dig_num) 
+print("Ваше двухзначное число: ", sec_num) 
