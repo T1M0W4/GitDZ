f02c459e3ab3edc2d8e49d97e47e06a0bb50e8b3
T1M0W4 <150163671+T1M0W4@users.noreply.github.com>
2023-11-21 21:46:07 +0600

    CW_Back(11.21.2023)v0.1

---
+++ CW_Back/CWBack(11.21.2023)/vitya1.py
@@ -0,0 +1,21 @@
+cost = 1500
+
+#if cost 1000:
+#    print("No discount")
+#elif cost < 2000:
+#    print("Discount 2%")
+#elif cost < 5000:
+#    print("Discount 5%")
+#else:
+#    print("Discount 10%")
+
+print
+
+if cost:
+    print("No discount")
+elif cost < 2000:
+    print("Discount 2%")
+elif cost < 5000:
+    print("Discount 5%")
+else:
+    print("Discount 10%")
