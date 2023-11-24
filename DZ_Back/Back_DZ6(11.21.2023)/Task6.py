846a81cb71cbe7942469c0b847dc07e677741e51
T1M0W4 <150163671+T1M0W4@users.noreply.github.com>
2023-11-24 13:04:10 +0600

    DZ6_Back(11.21.2023)v.0.2

---
+++ DZ_Back/Back_DZ6(11.21.2023)/Task6.py
@@ -0,0 +1,22 @@
+user_purch = input("На какую сумму, вы хотите совершить покупку?: ")
+
+try:
+    purch = int(user_purch)
+except ValueError:
+    print("Вы ввели не целое число!")
+else:
+    if purch < 200:
+        print("Сумма оплаты без скидки!", purch)
+    elif 300 > purch >= 200: 
+        disc = (purch*3)/100
+        stat = purch - disc 
+        print("Сумма оплаты со скидкой в 3%:", stat)
+    elif 500 > purch >= 300:
+        disc = (purch*5)/100
+        stat = purch - disc
+        print("Сумма оплаты со скидкой в 5%:", stat)
+    elif purch >= 500:
+        disc = (purch*7)/100
+        stat = purch - disc
+        print("Сумма оплаты со скидкой в 7%:", stat)
+   
