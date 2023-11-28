53b3a4ffefc116e844fe5be1eba03844e2f0e9ef
T1M0W4 <150163671+T1M0W4@users.noreply.github.com>
2023-11-28 21:47:39 +0600

    CWBack(11.28.2023)v0.1

---
+++ CW_Back/CWBack(11.28.2023)/CWBACK1.py
@@ -0,0 +1,9 @@
+for i in "python":
+    if i == "o" or i == "y":
+        continue
+    if i == "n":
+        break
+    print("Буква -",i)
+else:
+    print("Буквы кончились")
+
---
+++ DZ_Back/Back_DZ7(11.21.2023)/Task2.py
@@ -0,0 +1,22 @@
+num1 = ""
+num2 = ""
+
+buff_num = 1
+
+while not int() and num2 != int(num2):
+    num1 = int(input("Введите первое число: "))
+    
+        
+    num2 = input("Введите второе число: ")
+    try:
+        num2 = int(num2)
+    except ValueError:
+        print("Вы ввели не число!")
+
+print("zaebok")
+
+   
+
+
+    
+    
