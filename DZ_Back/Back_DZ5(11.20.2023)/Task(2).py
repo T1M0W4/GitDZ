1bfd5d6ca719d79301c483c65f3d8156c309eacf
T1M0W4 <150163671+T1M0W4@users.noreply.github.com>
2023-11-29 20:04:22 +0600

    DZ5_Back(11.29.2023)v0.3

--- DZ_Back/Back_DZ5(11.20.2023)/Task(2).py
+++ DZ_Back/Back_DZ5(11.20.2023)/Task(2).py
@@ -4,20 +4,7 @@ try:
     float_num = float(user_float_num)
 except ValueError:
     print("Вы ввели не дробное число!")
-<<<<<<< HEAD
     exit()
-=======
-else:
-    try:
-        user_int_num = input("Количество дробей в целом числе:")
-        #VN: ^^^^^^^^^^^^^^^^^ эта строка в блоке try не нужна - она не вызывает исключения
-        
-        int_num = int(user_int_num)
-    except ValueError:
-        print("Вы ввели не целое число!")
-    else:
-        user_round_num = round(float_num, int_num)
->>>>>>> 53b3a4ffefc116e844fe5be1eba03844e2f0e9ef
 
 user_int_num = input("Количество дробей в целом числе:")
 
@@ -31,6 +18,3 @@ except ValueError:
 user_round_num = round(float_num, int_num)
 
 print("Округление:", user_round_num)
-    
-    
-   
