b8e67f86871253317846f62452ed39a95b155e51
T1M0W4 <150163671+T1M0W4@users.noreply.github.com>
2023-12-18 21:45:26 +0600

    CW_Back(12.18.2023)

---
+++ CW_Back/CWBack(12.18.2023)/CW5.py
@@ -0,0 +1,12 @@
+def con_num(a,b,c):
+    a=str(a)
+    b=str(b)
+    c=str(c)
+    wew = (a+b+c)
+    kek = int(wew)
+    return kek
+
+
+
+res=con_num(6,3,8)
+print(res)
---
+++ DZ_Back/Новая папка/CW4.py
@@ -0,0 +1,6 @@
+a = int(input("$"))
+b = int(input("%"))
+
+c = a-((a*b)/100)
+
+print("Скидка ебать:%",round(c))
