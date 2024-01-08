6b20ee2c958f9a9310692d4a887af9654f5c3aca
T1M0W4 <150163671+T1M0W4@users.noreply.github.com>
2024-01-08 21:44:32 +0600

    CWBack(08.01.2024)v0.1

---
+++ CW_Back/CWBack(01.08.2024)/CW1/main.py
@@ -0,0 +1,9 @@
+# from sums import get_sum
+
+# temp = get_sum((7,4,9,-1,3))
+# print(f"Сумма = {temp}")
+
+import sums
+
+temp = sums.get_sum((7,4,9,-1,3))
+print(f"Сумма = {temp}")
---
+++ CW_Back/CWBack(01.08.2024)/CW1/sums.py
@@ -0,0 +1,5 @@
+def get_sum(numbers: list|tuple) -> float|int:
+    result = 0
+    for num in numbers:
+        result += num
+    return result
