63ed4e2676e2d2ddc1e2abe519bd2f482a753f6c
T1M0W4 <150163671+T1M0W4@users.noreply.github.com>
2024-01-16 21:44:23 +0600

    CWBack(01.16.2024)

--- DZ_Back/Back_DZ19(01.16.2024)/supergame/main.py
+++ DZ_Back/Back_DZ19(01.16.2024)/supergame/main.py
@@ -1,11 +1,18 @@
 import pygame as pg
+import controls
+import player
 
+FPS = 50
 pg.init()
+clock = pg.time.Clock()
 pg.display.set_caption("Magic Mastery Boss")
 icon = pg.image.load('resources/icons/robe.png')
 pg.display.set_icon(icon)
+screen = pg.display.set_mode((1024, 768))
+bg_color = "gray"
+me = player.init((24, 34), (512-25, 384-25), "magenta")
+bg = pg.image.load('resources/background/dunge.png')
 
-screen = pg.display.set_mode((800, 600))
 
 running = True
 while running:
@@ -13,8 +20,14 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-    screen.fill((77, 155, 161))
+    
+    controls.check()        
+    screen.blit(bg, (300, 300))
+    player.update(me)
+    screen.blit(me, player.my_position)
     pg.display.update()
+    clock.tick(FPS)
+    
 pg.quit()   
 
 
