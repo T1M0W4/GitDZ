0934a6be7964762f41c51a21f7f907d887cf28cf
T1M0W4 <150163671+T1M0W4@users.noreply.github.com>
2024-02-23 21:47:02 +0600

    cw

--- CW_Back/CWBack2024/CWBack(02.22.2024)/netsender.py
+++ CW_Back/CWBack2024/CWBack(02.22.2024)/netsender.py
@@ -2,9 +2,6 @@ import socket
 
 
 class Sender:
-    port = 0
-    sock = None
-
     def __init__(self, port):
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -13,3 +10,6 @@ class Sender:
         data = message.encode('utf8')
         addr = (dest_ip, self.port)
         self.sock.sendto(data, addr)
+        print(f'Отправлено к {addr} : {data}')
+
+    
